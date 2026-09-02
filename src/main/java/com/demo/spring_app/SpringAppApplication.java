07ef6af65d526bb7511e18d51ae44bab234619e3
SourabhBabhare <sourabhbabhare041@gmail.com>
2026-09-02 19:44:34 +0530

    second commit

--- src/main/java/com/demo/spring_app/SpringAppApplication.java
+++ src/main/java/com/demo/spring_app/SpringAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringAppApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
